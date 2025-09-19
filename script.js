b74a2fb1592e3a0e6c3c47874b11f242b21331ca
dayfordreamer-cell <dayfordreamer@gmail.com>
2025-09-19 12:48:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById("bookingForm").addEventListener("submit", async function
   const responseMessage = document.getElementById("responseMessage");
 
   try {
-    const response = await fetch("https://script.google.com/macros/s/AKfycbwSL-ppxMaFSjQr3qZZgL6LbN5IuhfjhFHYnRb4DvWWuNo613bUvPu_xQULeV75dZxV/exec", {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbxiklMGq-dKktUr3KAFniagBDLK0NnidbZS3oMXJMNF2hST1hfdJD1vt6lp9Sc806U4/exec", {
       method: "POST",
       body: new URLSearchParams(data),
     });
